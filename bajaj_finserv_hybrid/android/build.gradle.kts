ad86283360ef59630fc939d4f562d88537073bb1
Ayush-Charjan <ayushmcharjan@gmail.com>
2026-07-19 13:22:01 +0530

    some changes

--- bajaj_finserv_hybrid/android/build.gradle.kts
+++ bajaj_finserv_hybrid/android/build.gradle.kts
@@ -1,3 +1,6 @@
+// 1. First, set up the root build directory redirect
+rootProject.layout.buildDirectory.value(rootProject.layout.projectDirectory.dir("../build"))
+
 allprojects {
     repositories {
         google()
@@ -5,10 +8,16 @@ allprojects {
     }
 }
 
+// 2. Consolidate subproject configurations into a single block
 subprojects {
+    // Redirect each subproject (like plugins) into the root build folder
+    val newSubprojectBuildDir = rootProject.layout.buildDirectory.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Ensure the main :app evaluates first
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
+}   
